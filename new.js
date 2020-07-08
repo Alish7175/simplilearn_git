6fe77e731c4ced8dd0b2a18976db5885c3ebba39
C:\Users\ashok\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code.desktop <alishmadhukar75@gmail.com>
2020-07-08 23:52:13 +0530

    4th july and 5th july

--- new.js
+++ new.js
@@ -1,4 +1,10 @@
-function dataType(){
-    
 
+
+//datatype teller//
+
+function datatype(){
+    let ask = prompt("write anything to know it's datatype");
+    let ans = typeof ask;
+    document.getElementById("write").innerHTML =
+    `Datatype of what you have written is ${ans} `;
 }
