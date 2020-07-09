397fd083c46c32f531ec687fd522f77c2bf0194f
C:\Users\ashok\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code.desktop <alishmadhukar75@gmail.com>
2020-07-09 21:07:08 +0530

    fruits example

--- new.js
+++ new.js
@@ -14,7 +14,17 @@ var obj = {
     "empName":"sara",
     "empSalary": 99999,
 }
+obj.empId = 201;
 document.getElementById("pre_details").innerHTML =
     `The details of employee ${obj.empName} are as follows:  `;
     document.querySelector(".details").innerHTML =
     `Employee ID: ${obj.empId}<br>Employee Name: ${obj.empName}<br>Employee Salary: ${obj.empSalary}  `;    
+
+let fruits = ["apple" , "mango" , "banana" , "pumpkin"];
+let realFruits = fruits;
+realFruits[3] = "orange";
+console.log(fruits);    
+
+document.querySelector(".realFruits").innerHTML =
+    `Pumpkin changed to ${fruits[3]} by creating reference  `;    
+
