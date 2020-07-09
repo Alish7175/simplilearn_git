65ba5786b9a1534136d17592bab0f00ad8f56262
C:\Users\ashok\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code.desktop <alishmadhukar75@gmail.com>
2020-07-09 16:52:42 +0530

    employee_example

--- new.js
+++ new.js
@@ -8,3 +8,13 @@ function datatype(){
     document.getElementById("write").innerHTML =
     `Datatype of what you have written is ${ans} `;
 }
+
+var obj = {
+    "empId": 101,
+    "empName":"sara",
+    "empSalary": 99999,
+}
+document.getElementById("pre_details").innerHTML =
+    `The details of employee ${obj.empName} are as follows:  `;
+    document.querySelector(".details").innerHTML =
+    `Employee ID: ${obj.empId}<br>Employee Name: ${obj.empName}<br>Employee Salary: ${obj.empSalary}  `;    
